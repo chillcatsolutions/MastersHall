63712ebfcf8eb08d4f769772edc0076716e5f6a8
chillcatsolutions <chillcatsolutions@gmail.com>
2025-12-27 15:41:41 +0100

    Changed character scale and displaying both character and background

--- Game1.cs
+++ Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+
 namespace MastersHall
 {
     public class Game1 : Game
@@ -10,9 +11,13 @@ namespace MastersHall
         private SpriteBatch _spriteBatch;
         private RenderTarget2D _renderTarget;
 
-        public float scale = 0.44444f;
+        public float scale = 1f;
+        public float characterScale = 0.25f;
 
         Texture2D character1;
+        Texture2D background;
+
+        Vector2 playerPosition;
 
         public Game1()
         {
@@ -35,33 +40,38 @@ namespace MastersHall
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             
             character1 = Content.Load<Texture2D>("chatgpt_character_1");
+            background = Content.Load<Texture2D>("images/backgrounds/reference_background");
 
             _renderTarget = new RenderTarget2D(
                 GraphicsDevice,
                 1920,
                 1080);
 
+            
+            
+            playerPosition = new Vector2((_renderTarget.Width / 2) - (character1.Width * characterScale / 2), (_renderTarget.Height / 2) - (character1.Height * characterScale / 2));
+
+
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            scale = 1F / (1080F / _graphics.GraphicsDevice.Viewport.Height);
 
+            scale = 1F / (1080F / _graphics.GraphicsDevice.Viewport.Height);
             GraphicsDevice.SetRenderTarget(_renderTarget);
 
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(character1, Vector2.Zero, Color.White);
+            _spriteBatch.Draw(background, Vector2.Zero, Color.White);
+            _spriteBatch.Draw(texture: character1, position: playerPosition, sourceRectangle: null, color: Color.White, rotation: 0f, origin: Vector2.Zero, scale: characterScale, effects: SpriteEffects.None, layerDepth: 0f);
+
 
             _spriteBatch.End();
 
